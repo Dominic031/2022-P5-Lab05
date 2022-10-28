5f4f06c8b109e69d143da625892848594a1f6915
dom <dominic.cimaglia@vtsd.com>
2022-10-28 11:25:19 -0400

    Lab

--- Lab05vst.java
+++ Lab05vst.java
@@ -17,5 +17,56 @@ public class Lab05vst extends Applet
         int x2 = 990;
         int y2 = 640;
         g.drawRect(10,10,width,height);
+        x1= 10;
+        y1 = 640;
+        x2 =990;
+        y2 = 625;
+
+        for ( int x=0; x < 51; x++) {
+            g.drawLine(x1, y1, x2, y2);
+            x1 += 19;
+            y2 -= 12;
+
+        }
+        x1= 990;
+        y1 = 640;
+        x2 =10;
+        y2 = 625;
+
+        for ( int x=0; x < 51; x++) {
+            g.drawLine(x1, y1, x2, y2);
+            x1 -= 19;
+            y2 -= 12;
+
+        }
+        x1 = 10;
+        y1 = 10;
+        x2 =990;
+        y2 = 10;
+
+
+        for ( int x=0; x < 51; x++) {
+            g.drawLine(x1, y1, x2, y2);
+            x1 += 20;
+            y2 += 13;
+
+
+        }
+        x1 = 10;
+        y1 = 10;
+        x2 =990;
+        y2 = 10;
+
+
+        for ( int x=0; x < 51; x++) {
+            g.drawLine(x1, y1, x2, y2);
+            x2 -=20 ;
+            y1 += 12;
+
+
+        }
+        }
     }
-}
+
+
+
